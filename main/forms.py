6ca36f60823eb771a8c93833054601af7e2e82b3
Maxim Chavlytko <phobosxiii@yandex.ru>
2016-01-24 16:17:13 +0700

    Fix incorrect trimming username and password

--- main/forms.py
+++ main/forms.py
@@ -42,10 +42,12 @@ class ContactForm(forms.Form):
 
 class AuthForm(AuthenticationForm):
     def clean(self):
-        username = self.cleaned_data.get('username').strip()
-        password = self.cleaned_data.get('password').strip()
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
 
         if username and password:
+            username = username.strip()
+            password = password.strip()
             self.user_cache = authenticate(username=username,
                                            password=password)
             if self.user_cache is None:
